4ecde55a5d546300067c05a4b1a27d7612324a20
tesseir <tesseir.edu@gmail.com>
2023-01-11 20:18:18 -0600

    js code

--- script.js
+++ script.js
@@ -1,23 +1,73 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+// ? ls = local storage
+
+
+//writes to local storage
+function writels(key){
+ 
+  let value = ""
+  value = $(`#t-${key}`).val()
+  localStorage.setItem(key, value)
+
+}
+
+window.writels = writels
+
+
+//writes from local storage
+function readls(key){
+  let value = ""
+  value = localStorage.getItem(key)
+  if(value == null){
+    return""
+  } else {
+    return value
+  }
+}
+
+window.readls = readls
+
+
+//fills timeblocks with unique id and also its coresponding text and "past" "present "future" value.
+function createTimeBlock(index){
+
+  let timeClass = ""
+  let timeHours = new Date().getHours()
+
+  if (index == timeHours) {
+    timeClass = 'present'
+  } else if (index > timeHours) {
+    timeClass = 'future'
+  } else {
+    timeClass = 'past'
+  }
+
+  let text = readls(index)
+
+  return `<div id="${index}" class="row time-block ${timeClass}">
+  <div class="col-2 col-md-1 hour text-center py-3">${index}:00</div>
+  <textarea class="col-8 col-md-10 description" rows="3" id="t-${index}">${text}</textarea>
+  <button onclick="window.writels(${index})" class="btn saveBtn col-2 col-md-1" aria-label="save">
+    <i class="fas fa-save" aria-hidden="true"></i>
+  </button>
+</div>`
+}
+
+$(document).ready(function() { 
+
+  //generates time bases on system time, and creates timeblocks.
+  $('#currentDay').text(new Date().toLocaleString())
+  
+  let timeBlocks = []
+
+  for(let i = 0; i < 24; i++){
+    timeBlocks.push(createTimeBlock(i))
+  }
+
+  $('#timeblock-Parent').html(timeBlocks.join("")) 
+
+
 });
+
+
+
+
